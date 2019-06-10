7976ac26bf7d7b46efcd7e88d3e1b12427e4e97b
culberbn <culberbn@mail.uc.edu>
2019-06-10 14:40:11 -0400

    main write

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,5 +1,92 @@
 #include <iostream>
+#include "Lab_4\classes\classes.h"
+using namespace std;
 
 int main(){
-    
-}
+    //main for classes.cpp
+    //evently there will be one for stack and queue as well but in linked list versions
+
+    bool stop = false;
+    int choice;
+    student input, temp;
+    classes myinput;
+
+    cout << "Set Size --------------------------- 1" << endl;
+    cout << "addItem ---------------------------- 2" << endl;
+    cout << "getItem ---------------------------- 3" << endl;
+    cout << "isEmpty ---------------------------- 4" << endl;
+    cout << "isFull ----------------------------- 5" << endl;
+    cout << "size ------------------------------- 6" << endl;
+    cout << "clearList -------------------------- 7" << endl;
+    cout << "QUIT ------------------------------- 0" << endl;
+
+    while(!stop){
+        cout << "::> ";
+        cin >> choice;
+
+        switch(choice){
+            case(0):
+                stop = true;
+                break;
+            case(2):
+                fflush(stdin);
+                cout << "First Name: ";
+                getline(cin, input.firstName);
+                fflush(stdin);
+                cout << "Last Name: ";
+                getline(cin, input.lastName);
+                fflush(stdin);
+                cout << "M-Number: ";
+                cin >> input.m_number;
+                fflush(stdin);
+                cout << "Birthday (mm/dd/yy): ";
+                getline(cin, input.birthday);
+                fflush(stdin);
+                cout << "GPA: ";
+                cin >> input.gpa;
+                fflush(stdin);
+                myinput.addItem(input);
+                break;
+            case(3):
+                temp = myinput.getItem();
+                cout << "FirstName: " << temp.firstName << endl;
+                cout << "LastName: " << temp.lastName << endl;
+                cout << "M-number: " << temp.m_number << endl; 
+                cout << "Birthday: " << temp.birthday << endl;
+                cout << "GPA: " << temp.gpa << endl << endl;
+                break;
+            case(4):
+                if(myinput.isEmpty()){
+                    cout << "list is empty" << endl;
+                }else{
+                    cout << "list is not empty" << endl;
+                }
+                break;
+            case(5):
+                if(myinput.isFull()){
+                    cout << "list is full" << endl;
+                }else{
+                    cout << "list is not full" << endl;
+                }
+                break;
+            case(6):
+                cout << "size of list: " << myinput.size() << endl;
+                break;
+            case(7):
+                myinput.clearList();
+                cout << "list is cleared" << endl;
+                break;
+            // case(1):
+            //     cin >> input;
+            //     classes myinput = new classes(input);
+            }
+
+
+
+        }
+
+
+
+
+
+    }
