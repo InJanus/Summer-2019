31f29c0c0b23bf6a402d6511a6cd390f590c36b5
barkergs <barkergs@mail.uc.edu>
2019-06-13 08:56:37 -0400

    Random Array

--- Lab_5/sort.cpp
+++ Lab_5/sort.cpp
@@ -1,6 +1,19 @@
+#include <stdio.h>      // printf, scanf, puts, NULL 
+#include <stdlib.h>     // srand, rand 
+#include <time.h>       // time 
+
 //this is where the sort functions will go
 
-//radom genaration
+//random genaration
+int *randomGenerate(int n){
+    int myArray[10];
+
+    for(int i=0;i<n;i++){
+        myArray[i] = rand() % n +1;
+    }
+
+    return myArray;
+}
 //Bubble sort - brian
 //insertion sort -greg
 //merge sort - brian
