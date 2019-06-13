d84a5a1e692e256528bf81c3bb0fa98107dfdc5b
culberbn <culberbn@mail.uc.edu>
2019-06-13 08:30:22 -0400

    tasks for team

--- Lab_5/sort.cpp
+++ Lab_5/sort.cpp
@@ -1,1 +1,11 @@
 //this is where the sort functions will go
+
+//radom genaration
+//Bubble sort - brian
+//insertion sort -greg
+//merge sort - brian
+//quick sort - greg
+//counting sort - brian
+//radix sort - greg
+
+
