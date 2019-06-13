ea2a5a9b62486b162c4bb667cb35ef50f6925231
culberbn <culberbn@mail.uc.edu>
2019-06-13 09:03:04 -0400

    print and bubble sort

--- Lab_5/sort.cpp
+++ Lab_5/sort.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 //this is where the sort functions will go
 
 //radom genaration
@@ -8,4 +11,24 @@
 //counting sort - brian
 //radix sort - greg
 
+int* bubbleSort(int *items){
+    int ammountSorted = 0;
+    int size = int(sizeof(items));
+    while(ammountSorted < size){
+        for(int i = 0; i < size-ammountSorted-1; i++){
+            if(items[i+1] < items[i]){
+                //switch the items
+                int temp = items[i];
+                items[i] = items[i+1];
+                items[i+1] = temp;
+            }
+        }
+        ammountSorted++;
+    }
+}
 
+void printItems(int *items){
+    for(int i =0; i < int(sizeof(items)); i++){
+        cout << "i: " << i << " == " << items[i] << endl;
+    }
+}
