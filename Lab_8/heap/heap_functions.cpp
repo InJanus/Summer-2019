7c8873f63b379bfa7d7e3dad2494d61bb2c40571
barkergs <barkergs@mail.uc.edu>
2019-07-18 09:45:37 -0400

    Start

--- Lab_8/heap/heap_functions.cpp
+++ Lab_8/heap/heap_functions.cpp
@@ -2,3 +2,21 @@
 #include<iostream>
 using namespace std;
 
+int parent(int node) { 
+    return floor(node-1)/2; 
+} 
+  
+// to get index of left child of node at index i 
+int left(int node) { 
+    return (2*node + 1); 
+} 
+  
+// to get index of right child of node at index i 
+int right(int node) { 
+    return (2*node + 2); 
+}
+
+
+//for remove we have to look at the children and replace with the greater child and continue to do that.
+
+//heap up 
