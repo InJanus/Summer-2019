1388b3c3fd045c4300e4d1cfae6416bb123bd54e
barkergs <barkergs@mail.uc.edu>
2019-07-22 14:11:35 -0400

    Insert

--- Lab_8/heap/heap_functions.cpp
+++ Lab_8/heap/heap_functions.cpp
@@ -16,7 +16,18 @@ int right(int node) {
     return (2*node + 2); 
 }
 
-
+void heap::insert(int node){
+    if (head == nullptr){
+        head =new Node;
+        head->data = node;
+    }
+    else{
+        tail -> next = new Node;
+        tail -> next -> data = node;
+        tail -> node;
+    }
+}
 //for remove we have to look at the children and replace with the greater child and continue to do that.
+
 
 //heap up 
