7f107dbde86fdd010263bc3d5fe7617a6355526c
barkergs <barkergs@mail.uc.edu>
2019-07-29 12:17:47 -0400

    main

--- Lab_9/main.cpp
+++ Lab_9/main.cpp
@@ -12,13 +12,11 @@
 	bool quit = false;
 	int vert1 = 0;
 	int vert2 = 0;
-	 
-	// To use the text file method comment out this main function and follow the 
-	// steps on line 91 and 92, also note input methods are on the switch-case.
+	
 	int main()
 	{
 		
-		DirectedGraph graph;
+		graph graph;
 		while (quit == false)
 		{
 				std::cout << "Press 1 to Add an edge." << std::endl;
@@ -26,12 +24,13 @@
 				std::cout << "Press 3 to Find an edge." << std::endl;								
 				std::cout << "Press 4 to Find the out edge." << std::endl;
 				std::cout << "Press 5 to Find the in edge" << std::endl;
-				std::cout << "Press 6 to Print Array." << std::endl;
-				std::cout << "Press 7 to Quit." << std::endl;
+                std::cout << "Press 6 to Quit." << std::endl;
+				std::cout << "Press 7 to Print Array." << std::endl;
 				std::cin >> command;
 
-			if (command == 1)
+			switch(command)
 			{
+            case (1):
 				std::cout << "Enter the two vertices that are on connected to the desired edge." << std::endl;
 				std::cout << "Enter vertice one." << std::endl;
 				std::cin >> vert1;
@@ -39,10 +38,9 @@
 				std::cin >> vert2;
 
 				graph.addEdge(vert1, vert2);
+                break;
 
-			}
-			else if (command == 2)
-			{
+			case (2):
 				std::cout << "Enter the two vertices that are on connected to the desired edge." << std::endl;
 				std::cout << "Enter vertice one." << std::endl;
 				std::cin >> vert1;
@@ -50,10 +48,8 @@
 				std::cin >> vert2;
 
 				graph.removeEdge(vert1, vert2);
-
-			}
-			else if (command == 3)
-			{
+                break;
+			case (3):
 				std::cout << "Enter the two vertices that are on connected to the desired edge." << std::endl;
 				std::cout << "Enter vertice one." << std::endl;
 				std::cin >> vert1;
@@ -61,32 +57,29 @@
 				std::cin >> vert2;
 
 				graph.hasEdge(vert1, vert2);
-			}
-			else if (command == 4)
-			{
+                break;
+			case (4)
 
 				std::cout << "Enter the vertice that is connected to the desired edge." << std::endl;
 				std::cin >> vert1;
 
 				std::cout << graph.inEdges(vert1) << std::endl;
-
-			}
-			else if (command == 5)
-			{
+                break;
+			case (5):
 				std::cout << "Enter the vertice that is connected to the desired edge." << std::endl;
 				std::cin >> vert1;
 
 				std::cout << graph.outEdges(vert1) << std::endl;
-			}
-			else if (command == 6)
-			{
-				std::cout << "Printing Current Graph." << std::endl;
-				graph.printGraph();
-			}
-			else if (command == 7)
-			{
+                break;
+			case (6):
 				quit = true;
-			}
+                break;
+			case (7):
+                std::cout << "Printing Current Graph." << std::endl;
+				graph.printGraph();
+                break;
+            default:
+                std::cout << "Invalid Input"<< std::endl; 
 		}
 		quit = false;
 		
