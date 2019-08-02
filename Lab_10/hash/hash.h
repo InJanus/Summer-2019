b21d4441b1ae2f3910c50bfaaf9bb4b845528b80
SooriaAlagappan <52727238+SooriaAlagappan@users.noreply.github.com>
2019-08-02 15:32:05 -0400

    3 classes

--- Lab_10/hash/hash.h
+++ Lab_10/hash/hash.h
@@ -1,16 +1,42 @@
-using namespace std;
+#pragma once
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+const int T_S = 100;
+using namespace std;
 
-class hash{
-    private:
-        int size;
-        int hashfunction(string);
-        
-    public:
-        hash();
-        ~hash();
-        void addItem(string item);
-        string returnItem(string item);
-        string* getItem();
-        int getLength();
-        };
+
+class HashTable {
+public:
+	int k;
+	int v;
+	HashTable(int k, int v) {
+		this->k = k;
+		this->v = v;
+	}
+};
+
+class DelNode :public HashTable {
+private:
+	static DelNode* en;
+	DelNode() :HashTable(-1, -1) {}
+public:
+	static DelNode* getNode() {
+		if (en == NULL)
+			en = new DelNode();
+		return en;
+	}
+};
+
+
+class HashMapTable {
+private:
+	HashTable** ht;
+public:
+	HashMapTable();
+	int Hash(int);
+	void addItem(int, int);
+	int getItem(int);
+	void RemoveItem(int);
+	~HashMapTable();
+};
